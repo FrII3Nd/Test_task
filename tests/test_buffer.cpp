1edea82f00ec954af4ab3e941bda669b9ebde024
Спариш Владислав Дмитриевич <sparish_vd@peleng.jsc.local>
2024-10-30 12:33:01 +0300

    Add Iterator

--- tests/test_buffer.cpp
+++ tests/test_buffer.cpp
@@ -1,33 +1,33 @@
 #include <gtest/gtest.h>
 #include "CircularBuffer.hpp"
 
-TEST(RingBufferTest, Put_GET) {
+TEST(RingBufferTest, Push_Pop) {
 	CircularBuffer<int, 3> test;
 	EXPECT_EQ(test.max_size(), 3);
 	EXPECT_EQ(test.size(), 0);
-	test.put(1);
-	test.put(2);
-	test.put(3);
+	test.push(1);
+	test.push(2);
+	test.push(3);
 	EXPECT_EQ(test.size(), 3);
-	EXPECT_EQ(test.get(), 1);
+	EXPECT_EQ(test.pop(), 1);
 }
 TEST(RingBufferTest, Overflow) {
 	CircularBuffer<int, 3> test;
-	test.put(1);
-	test.put(2);
-	test.put(3);
+	test.push(1);
+	test.push(2);
+	test.push(3);
 	EXPECT_EQ(test.size(), 3);
-	test.put(4);
+	test.push(4);
 	EXPECT_EQ(test.size(), 3);
-	EXPECT_EQ(test.get(), 2);
-	test.put(5);
-	EXPECT_EQ(test.get(), 3);
+	EXPECT_EQ(test.pop(), 2);
+	test.push(5);
+	EXPECT_EQ(test.pop(), 3);
 }
 TEST(RingBufferTest, Remove) {
 	CircularBuffer<int, 3> test;
-	test.put(1);
-	test.put(2);
-	test.put(3);
+	test.push(1);
+	test.push(2);
+	test.push(3);
 	EXPECT_EQ(test.size(), 3);
 	test.remove();
 	EXPECT_EQ(test.size(), 2);
@@ -37,71 +37,52 @@ TEST(RingBufferTest, Remove) {
 }
 TEST(RingBufferTest, Float) {
 	CircularBuffer<float, 3> test;
-	test.put(3.1f);
-	test.put(2.2f);
-	test.put(1.3f);
-	EXPECT_EQ(test.get(), 3.1f);
+	test.push(3.1f);
+	test.push(2.2f);
+	test.push(1.3f);
+	EXPECT_EQ(test.pop(), 3.1f);
 	test.remove();
-	EXPECT_EQ(test.get(), 2.2f);
+	EXPECT_EQ(test.pop(), 2.2f);
 	test.remove();
-	EXPECT_EQ(test.get(), 1.3f);
+	EXPECT_EQ(test.pop(), 1.3f);
 }
 TEST(RingBufferTest, Char) {
 	CircularBuffer<char, 3> test;
-	test.put('a');
-	test.put('b');
-	test.put('c');
-	EXPECT_EQ(test.get(), 'a');
+	test.push('a');
+	test.push('b');
+	test.push('c');
+	EXPECT_EQ(test.pop(), 'a');
 	test.remove();
-	EXPECT_EQ(test.get(), 'b');
+	EXPECT_EQ(test.pop(), 'b');
 	test.remove();
-	EXPECT_EQ(test.get(), 'c');
+	EXPECT_EQ(test.pop(), 'c');
 }
 TEST(RingBufferTest, Bool) {
 	CircularBuffer<bool, 3> test;
-	test.put(0);
-	test.put(0);
-	test.put(1);
-	EXPECT_EQ(test.get(), 0);
+	test.push(0);
+	test.push(0);
+	test.push(1);
+	EXPECT_EQ(test.pop(), 0);
 	test.remove();
-	EXPECT_EQ(test.get(), 0);
+	EXPECT_EQ(test.pop(), 0);
 	test.remove();
-	EXPECT_EQ(test.get(), 1);
+	EXPECT_EQ(test.pop(), 1);
 }
-//TEST(RingBufferTest, Complex) {
-//	CircularBuffer<int, 5> test;
-//	test.put(1);
-//	test.put(2);
-//	test.put(3);
-//	test.put(4);
-//	test.put(5);
-//	EXPECT_EQ(test.get(), 1);
-//	EXPECT_EQ(test.get(), 2);
-//	EXPECT_EQ(test.get(), 3);
-//	EXPECT_EQ(test.get(), 4);
-//	EXPECT_EQ(test.get(), 5);
-//	test.put(1);
-//	test.put(2);
-//	test.put(3);
-//	test.put(4);
-//	test.put(5);
-//	test.put(6);
-//	EXPECT_EQ(test.get(), 2);
-//	test.remove();
-//	test.remove();
-//	test.remove();
-//	EXPECT_EQ(test.get(), 6);
-//	test.put(1);
-//	test.put(2);
-//	test.put(3);
-//	test.put(4);
-//	test.put(5);
-//	test.put(6);
-//	test.put(7);
-//	test.put(8);
-//	EXPECT_EQ(test.get(), 4);
-//	EXPECT_EQ(test.get(), 5);
-//	EXPECT_EQ(test.get(), 6);
-//	EXPECT_EQ(test.get(), 7);
-//	EXPECT_EQ(test.get(), 8);
-//}
+TEST(RingBufferTest, Iterator) {
+	CircularBuffer<int, 5> test;
+	test.push(1);
+	test.push(2);
+	test.push(3);
+	test.push(4);
+	test.push(5);
+	Iterator<int, 5> it(test);
+	EXPECT_EQ(*it, 1);
+	EXPECT_EQ(++it, 2);
+	EXPECT_EQ(++it, 3);
+	EXPECT_EQ(++it, 4);
+	EXPECT_EQ(--it, 3);
+	EXPECT_EQ(--it, 2);
+	EXPECT_EQ(--it, 1);
+	EXPECT_EQ(it.Begin(), 1);
+	EXPECT_EQ(it.End(), 5);
+}
