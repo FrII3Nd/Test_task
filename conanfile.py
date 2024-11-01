776cedb8a662dfc83a41fb849ff8452eb3cc0ae7
Спариш Владислав Дмитриевич <sparish_vd@peleng.jsc.local>
2024-11-01 16:27:33 +0300

    delete layout

--- conanfile.py
+++ conanfile.py
@@ -7,13 +7,10 @@ class MyProjectConan(ConanFile):
     version = "1.0"
     settings = "os", "compiler", "build_type", "arch"
     generators = "CMakeToolchain", "CMakeDeps"
-    requires = "gtest/1.13.0" 
+    requires = "gtest/1.14.0"
 
     def build(self):
         cmake = CMake(self)
         cmake.configure()
         cmake.build()
 
-
-    def layout(self):
-        self.folders.build = "build"
