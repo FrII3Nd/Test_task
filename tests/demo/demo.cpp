f6f5fd2ba7aa62d803a8e5d575009dbaa0c06899
Спариш Владислав Дмитриевич <sparish_vd@peleng.jsc.local>
2024-11-05 14:57:42 +0300

    New demo

--- tests/demo/demo.cpp
+++ tests/demo/demo.cpp
@@ -2,18 +2,35 @@
 #include <iostream>
 
 int main() {
-    CircularBuffer<int, 5> test;
-    test.push(1);
-    test.push(2);
-    test.push(3);
-    test.push(4);
-    test.push(5);
-    Iterator<int, 5> it(test);
-    std::cout <<*it << " ";
-    std::cout << it++ << " ";
-    std::cout << it++ << " ";
-    std::cout << ++it << " ";
-    std::cout << ++it << "\n";
-    system("pause");
+    CircularBuffer<int, 5> demo;
+    demo.push(0);
+    demo.push(1);
+    demo.push(2);
+    demo.push(3);
+    demo.push(4);
+    demo.push(5);
+    std::cout<<"\nSize - "<<demo.size()<<"\n";
+    auto it1 = demo.begin();
+    auto it2 = demo.end();
+    std::cout<<"\nBegin-->End: ";
+    for (it1; it1 != it2; it1++)
+    {
+        std::cout<<"\t"<<*it1;
+    }
+    std::cout<<"\t"<<*it2<<"\n";
+    auto it3 = demo.begin();
+    auto it4 = demo.end();
+    std::cout<<"\nEnd-->Begin: ";
+    for (it4; it4 != it3; it4--)
+    {
+        std::cout<<"\t"<<*it4;
+    }
+    std::cout<<"\t"<<*it3<<"\n\nPop():       ";
+    for(int i = 0; i < 5; i++)
+    {
+        std::cout<<"\t"<<demo.pop();
+    }
+    std::cout<<"\n\n";
+    std::cout<<"Size - "<<demo.size()<<"\n";
     return 0;
 }
