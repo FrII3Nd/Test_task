1cfcb7ee2da8bd0c34be5d67763d7bb964a1474b
Спариш Владислав Дмитриевич <sparish_vd@peleng.jsc.local>
2024-11-05 14:56:10 +0300

    Cpputest instead of gtest

--- conanfile.py
+++ conanfile.py
@@ -1,16 +1,31 @@
 from conan import ConanFile
-from conan.tools.cmake import CMake, CMakeToolchain, CMakeDeps
+from conan.tools.files import copy, load
+from conan.tools.cmake import CMakeToolchain, CMake, cmake_layout, CMakeDeps
+import os
 
 
 class MyProjectConan(ConanFile):
     name = "Test"
     version = "1.0"
     settings = "os", "compiler", "build_type", "arch"
-    generators = "CMakeToolchain", "CMakeDeps"
-    requires = "gtest/1.14.0"
+
+    generators = "CMakeDeps"
+
+    def requirements(self):
+        if not self.conf.get("tools.build:skip_test", default=False):
+            self.test_requires("cpputest/4.0")
+
+    def layout(self):
+        if not self.conf.get("tools.build:skip_test", default=False):
+            cmake_layout(self)
+
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.generate()
 
     def build(self):
         cmake = CMake(self)
         cmake.configure()
-        cmake.build()
-
+       # cmake.build()
+        if not self.conf.get("tools.build:skip_test", default=False):
+            cmake.test()
--- tests/test_buffer.cpp
+++ tests/test_buffer.cpp
@@ -1,68 +1,101 @@
-#include <gtest/gtest.h>
+#include "CppUTest/TestHarness.h"
 #include "CircularBuffer.hpp"
 
+TEST_GROUP(RingBufferTest)
+{
+};
 TEST(RingBufferTest, Push_Pop) {
 	CircularBuffer<int, 3> test;
-	EXPECT_EQ(test.max_size(), 3);
-	EXPECT_EQ(test.size(), 0);
+    CHECK_EQUAL(test.max_size(), 3);
+    CHECK_EQUAL(test.size(), 0);
 	test.push(1);
 	test.push(2);
 	test.push(3);
-	EXPECT_EQ(test.size(), 3);
-	EXPECT_EQ(test.pop(), 1);
+    CHECK_EQUAL(test.size(), 3);
+    CHECK_EQUAL(test.pop(), 1);
 }
 TEST(RingBufferTest, Overflow) {
 	CircularBuffer<int, 3> test;
 	test.push(1);
 	test.push(2);
 	test.push(3);
-	EXPECT_EQ(test.size(), 3);
+    CHECK_EQUAL(test.size(), 3);
 	test.push(4);
-	EXPECT_EQ(test.size(), 3);
-	EXPECT_EQ(test.pop(), 2);
+    CHECK_EQUAL(test.size(), 3);
+    CHECK_EQUAL(test.pop(), 2);
 	test.push(5);
-	EXPECT_EQ(test.pop(), 3);
+    CHECK_EQUAL(test.pop(), 3);
 }
 TEST(RingBufferTest, Float) {
 	CircularBuffer<float, 3> test;
 	test.push(3.1f);
 	test.push(2.2f);
 	test.push(1.3f);
-	EXPECT_EQ(test.pop(), 3.1f);
-	EXPECT_EQ(test.pop(), 2.2f);
-	EXPECT_EQ(test.pop(), 1.3f);
+    CHECK_EQUAL(test.pop(), 3.1f);
+    CHECK_EQUAL(test.pop(), 2.2f);
+    CHECK_EQUAL(test.pop(), 1.3f);
 }
 TEST(RingBufferTest, Char) {
 	CircularBuffer<char, 3> test;
 	test.push('a');
 	test.push('b');
 	test.push('c');
-	EXPECT_EQ(test.pop(), 'a');
-	EXPECT_EQ(test.pop(), 'b');
-	EXPECT_EQ(test.pop(), 'c');
+    CHECK_EQUAL(test.pop(), 'a');
+    CHECK_EQUAL(test.pop(), 'b');
+    CHECK_EQUAL(test.pop(), 'c');
 }
 TEST(RingBufferTest, Bool) {
 	CircularBuffer<bool, 3> test;
 	test.push(0);
 	test.push(0);
 	test.push(1);
-	EXPECT_EQ(test.pop(), 0);
-	EXPECT_EQ(test.pop(), 0);
-	EXPECT_EQ(test.pop(), 1);
+    CHECK_EQUAL(test.pop(), 0);
+    CHECK_EQUAL(test.pop(), 0);
+    CHECK_EQUAL(test.pop(), 1);
 }
-TEST(RingBufferTest, Iterator) {
+TEST(RingBufferTest, Iterator_incr_decr) {
 	CircularBuffer<int, 5> test;
 	test.push(1);
 	test.push(2);
 	test.push(3);
 	test.push(4);
 	test.push(5);
-	Iterator<int, 5> it(test);
-	EXPECT_EQ(*it, 1);
-	EXPECT_EQ(++it, 2);
-	EXPECT_EQ(it++, 3);
-	EXPECT_EQ(++it, 4);
-	EXPECT_EQ(it--, 3);
-	EXPECT_EQ(--it, 2);
-	EXPECT_EQ(--it, 1);
+    auto it = test.begin();
+    CHECK_EQUAL(*it, 1);
+    CHECK_EQUAL(++it, 2);
+    CHECK_EQUAL(it++, 3);
+    CHECK_EQUAL(++it, 4);
+    CHECK_EQUAL(it--, 3);
+    CHECK_EQUAL(--it, 2);
+    CHECK_EQUAL(--it, 1);
+}
+TEST(RingBufferTest, Iterator_compare) {
+    CircularBuffer<int, 3> test;
+    test.push(1);
+    test.push(2);
+    test.push(3);
+    auto it = test.begin();
+    auto it2 = test.end();
+    CHECK_FALSE(it==it2);
+    CHECK_TRUE(it!=it2);
+    it++;
+    CHECK_FALSE(it==it2);
+    it++;
+    CHECK_TRUE(it==it2);
+    CHECK_FALSE(it!=it2);
+}
+TEST(RingBufferTest, Iterator_ring) {
+    CircularBuffer<int, 3> test;
+    test.push(1);
+    test.push(2);
+    test.push(3);
+    test.push(4);
+    auto it = test.begin();
+    CHECK_EQUAL(*it, 2);
+    CHECK_EQUAL(++it, 3);
+    CHECK_EQUAL(it++, 4);
+    it = test.end();
+    CHECK_EQUAL(*it, 4);
+    CHECK_EQUAL(--it, 3);
+    CHECK_EQUAL(it--, 2);
 }
